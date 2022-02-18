58ebd29c2519e17b6f522e250c1d09476d11ad91
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-02-18 14:21:28 +0200

    first commit

---
+++ Flyweight/AllLibraries.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
+#include <ctime>
+#include <vector>
+#include <exception>
+#include <cstring> 
+#include <stdlib.h>
+#include <random>
+#include <ostream>
+#include <cmath>
+#include <chrono>
+using namespace std;
